c619de178d07e54cb91b2feb96a2040e07f25baa
Abin Babu <beingabin@gmail.com>
2025-08-04 22:28:07 +0530

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import AmbientAudio from "@/components/AmbientAudio";
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <AmbientAudio />
+      {/* Other page content */}
+    </div>
+  );
+}
